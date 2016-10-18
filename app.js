dcb06599598e8a1e1257cb393b319ba720f5b288
Spencer Norris <spencercnorris2017@gmail.com>
2016-10-17 22:38:42 -0400

    added old code to app.js

--- app.js
+++ app.js
@@ -223,3 +223,29 @@ if ( cloudantUrl ) {
 }
 
 module.exports = app;
+
+
+//ARTIFACTS FROM LAST VERSION, ADDED HERE FOR REFERENCE LATER -- Spencer
+/*
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+var credentials = JSON.parse(fs.readFileSync('./credentials.json', 'utf8'));
+var alchemy_language = watson.alchemy_language({'api_key': credentials['credentials']['apikey']});
+var _ = require('underscore');
+
+var parameters = {
+  extract: 'entities,keywords,concepts,taxonomy,typed-rels',
+  sentiment: 1,
+  maxRetrieve: 1,
+  model: 'en-news',
+  text: 'What is Hillary Clinton\'s opinion on the Syrian refugee crisis?'
+};
+
+alchemy_language.combined(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
+});
+
+*/
