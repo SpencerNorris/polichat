507267daf98e0db0676c47a2a26ed9ab4c0a222a
Spencer Norris <spencercnorris2017@gmail.com>
2016-10-17 21:08:29 -0400

    re-added sample interaction

--- app.js
+++ app.js
@@ -4,6 +4,12 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
+var watson = require('watson-developer-cloud');
+var fs = require('fs');
+var credentials = JSON.parse(fs.readFileSync('./credentials.json', 'utf8'));
+var alchemy_language = watson.alchemy_language({'api_key': credentials['credentials']['apikey']});
+var _ = require('underscore');
+
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
@@ -25,4 +31,21 @@ var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
+});
+
+
+
+var parameters = {
+  extract: 'entities,keywords,concepts,taxonomy,typed-rels',
+  sentiment: 1,
+  maxRetrieve: 1,
+  model: 'en-news',
+  text: 'What is Hillary Clinton\'s opinion on the Syrian refugee crisis?'
+};
+
+alchemy_language.combined(parameters, function (err, response) {
+  if (err)
+    console.log('error:', err);
+  else
+    console.log(JSON.stringify(response, null, 2));
 });
